62836251a8064b1075d1d2f7c9a0e1c0ad51bbc4
Jessy Trejo <keine.9010@gmail.com>
2023-01-20 18:52:51 -0600

    Added menu, their animations and responsive media queries

---
+++ js/audio.js
@@ -0,0 +1,19 @@
+const cardMercury = document.getElementsByClassName('card__planet-img');
+const audioMercury = new Audio("../assets/blablabla-82044.mp3");
+
+//cardMercury.addEventListener("click", () => {
+//    audioMercury.play();
+//});
+
+// Objects array: Planets + sfx url
+const planets = [
+    {
+        name: "Mercury",
+        audioUrl: "../assets/blablabla-82044.mp3"
+    },
+    {
+        name: "Venus",
+        audioUrl:""
+    }
+];
+
