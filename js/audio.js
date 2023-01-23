ecc4d45728be4112ebc2847b9f50068f4a41d510
Jessy Trejo <keine.9010@gmail.com>
2023-01-22 21:11:16 -0600

    Added animal pics and animals HTML page. Added a test in JS file to make animals sound when clicked.

--- js/audio.js
+++ js/audio.js
@@ -1,19 +1,6 @@
-const cardMercury = document.getElementsByClassName('card__planet-img');
-const audioMercury = new Audio("../assets/blablabla-82044.mp3");
+const animal = document.querySelector('.card__img');
+const sfx = new Audio("../assets/sfx/mooing-cow-122255.mp3");
 
-//cardMercury.addEventListener("click", () => {
-//    audioMercury.play();
-//});
-
-// Objects array: Planets + sfx url
-const planets = [
-    {
-        name: "Mercury",
-        audioUrl: "../assets/blablabla-82044.mp3"
-    },
-    {
-        name: "Venus",
-        audioUrl:""
-    }
-];
-
+animal.addEventListener("click", () => {
+    sfx.play();
+});
