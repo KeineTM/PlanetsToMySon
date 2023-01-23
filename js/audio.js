d5946b1d5a95521a0fd81473c1aa13e686832ade
Jessy Trejo <keine.9010@gmail.com>
2023-01-23 16:24:38 -0600

    Added new svg planet images, change look of index, and planets page

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,5 @@
 const animal = document.querySelector('.card__img');
-const sfx = new Audio("../assets/sfx/mooing-cow-122255.mp3");
+const sfx = new Audio("../assets/animals-sfx/mooing-cow-122255.mp3");
 
 animal.addEventListener("click", () => {
     sfx.play();
