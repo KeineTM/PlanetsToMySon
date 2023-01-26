9a603efffe2530402e6ed84afd3651c48d201c72
Jessy Trejo <keine.9010@gmail.com>
2023-01-25 18:37:20 -0600

    Updated animation in planets.html and added a json-server

--- js/planets.js
+++ js/planets.js
@@ -1,28 +1,40 @@
-// Variables
+// Variables -----------------------------------------------------------------------------
 const container = document.querySelector('.content'); // Container
 const planets = document.querySelectorAll('.content__list-item'); // Planets array
+
 let i = 0;
 
-// Methods
-const hide = (element) => {
-    element.classList.add('hide');
-}
+// Methods ---------------------------------------------------------------------------------
+const hide = (element) => element.classList.add('hide')
 
-const show = (element) => {
-    element.classList.remove('hide');
-}
+const show = (element) => element.classList.remove('hide');
 
-// Event Listener when click container
-container.addEventListener("click", () => {
-    if(planets[i].nextElementSibling) { // If the element have next in array
+const move = (x) => {
+    console.log(i)
+    if(i+x >= 0 && i+x < planets.length) { // If the index is within the boundaries of array
         hide(planets[i]); // Hide the actual element
-        show(planets[i].nextElementSibling); // Show next
-        i++; // Count +1
+        i+=x; // Count +1 or -1
+        show(planets[i]); // Show next or previous
+    } else if(i+x === planets.length) { // But, if index is out of the boundaries
+        hide(planets[i]);
+        i = 0; // Reset to zero
+        show(planets[i]);
     } else {
-        hide(planets[i]); // Hide the actual element
-        i = 0; // Reset count
-        show(planets[i]); // Show new actual element
+        hide(planets[i]);
+        i = planets.length-1; // Reset to max
+        show(planets[i]);
     }
+    
+}
+
+//------------------------------------------------------------------------------------------
+// Event Listener when click container
+//container.addEventListener('click', goNext);
+
+// Event Listener when press arrow keys on window
+window.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowRight') move(1);
+    else if(event.key === 'ArrowLeft') move(-1);
 });
 
 /**
